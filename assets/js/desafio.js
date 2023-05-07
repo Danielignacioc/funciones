e9dbc6c48557b0d44b415d6bb15146127280d93f
DanielIgnacioCarcamo <carcamodaniel.informatica@gmail.com>
2023-05-07 11:20:42 -0400

    arreglos

--- assets/js/desafio.js
+++ assets/js/desafio.js
@@ -1,9 +1,3 @@
-/* const e1 = document.getElementById("i1");
-const e2 = document.getElementById("i2");
-const e3 = document.getElementById("i3");
-const e4 = document.getElementById("i4");
- */
-
 function seleccionar(sId) {
   const eId = document.getElementById(sId);
   document.addEventListener("keydown", function (event) {
